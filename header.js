0ec5f46e6123a6827f7c319042d20d8e01037ba4
Marcus Bannerman <m.bannerman@gmail.com>
2021-01-06 10:45:34 +0000

    Fixed the notes, just proof reading them now

--- header.js
+++ header.js
@@ -1,2 +1,14 @@
-head.load("../reveal.js/css/reveal.css", "../reveal.js/css/theme/beige.css", "../MCBreveal.css", "../resources/Font-Awesome/css/font-awesome.min.css");
+document.getElementsByTagName("head")[0].insertAdjacentHTML(
+  "beforeend",
+  "<link rel=\"stylesheet\" href=\"../reveal.js/dist/reveal.css\" />"
+  +"<link rel=\"stylesheet\" href=\"../reveal.js/dist/theme/beige.css\" />"  
+  +"<link rel=\"stylesheet\" href=\"../MCBreveal.css\" />"  
+  +"<link rel=\"stylesheet\" href=\"../resources/Font-Awesome/css/font-awesome.min.css\" />"
+  +"<link rel=\"shortcut icon\" type=\"image/png\" href=\"favicon.png\">"
+);
 document.head.getElementsByTagName("link")[1].id="theme"
+
+navigator.getUserMedia = ( navigator.getUserMedia ||
+                       navigator.webkitGetUserMedia ||
+                       navigator.mozGetUserMedia ||
+                       navigator.msGetUserMedia);
