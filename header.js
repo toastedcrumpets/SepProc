527ed5b0547415591d8da1f2fb344ad79795cbfe
Marcus Bannerman <m.bannerman@gmail.com>
2024-09-03 09:27:47 +0100

    Fix for upgrade of reveal

--- header.js
+++ header.js
@@ -1,18 +1,18 @@
+
 document.getElementsByTagName("head")[0].insertAdjacentHTML(
   "beforeend",
   "<link rel=\"stylesheet\" href=\"../reveal.js/dist/reveal.css\" />"
-  +"<link rel=\"stylesheet\" href=\"plugins/drawer.css\" />"
-  +"<link rel=\"stylesheet\" href=\"../reveal.js/dist/theme/beige.css\" />"  
-  +"<link rel=\"stylesheet\" href=\"../MCBreveal.css\" />"  
+  +"<link rel=\"stylesheet\" href=\"../reveal.js/dist/theme/white.css\" />"  
   +"<link rel=\"stylesheet\" href=\"../resources/Font-Awesome/css/font-awesome.min.css\" />"
   +"<link rel=\"shortcut icon\" type=\"image/png\" href=\"favicon.png\">"
 );
+
 document.head.getElementsByTagName("link")[1].id="theme"
 
 navigator.getUserMedia = ( navigator.getUserMedia ||
-			   navigator.webkitGetUserMedia ||
-			   navigator.mozGetUserMedia ||
-			   navigator.msGetUserMedia);
+                       navigator.webkitGetUserMedia ||
+                       navigator.mozGetUserMedia ||
+                       navigator.msGetUserMedia);
 
 function UpdateQueryString(key, value, url) {
   if (!url) url = window.location.href;
